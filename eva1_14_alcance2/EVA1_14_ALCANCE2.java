e0b8d8234988e3566ae378e9cd63eae4d192355d
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-02-23 09:40:33 -0600

    Add files via upload

---
+++ eva1_14_alcance2/EVA1_14_ALCANCE2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_14_alcance2;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_14_ALCANCE2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
